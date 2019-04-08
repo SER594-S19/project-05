8f64b3338672993349719aefdd87f4242b3d9cf9
schaud28 <schaud28@asu.edu>
2019-04-08 12:09:42 -0700

    Added state machine functionality

--- faceEmotions/src/main/java/Client/Subscriber.java
+++ faceEmotions/src/main/java/Client/Subscriber.java
@@ -1,13 +1,15 @@
 package Client;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Observable;
-
+import com.opencsv.CSVWriter;
 
 /**
  * ThreadSubscriber. A generic subscriber; get data from a network connection;
@@ -23,6 +25,9 @@ public class Subscriber extends Observable implements Runnable {
   private String Ip;
   private int port;
   private String data;
+  private File file;
+  private int iter;
+
 
 
   public int getPort(){
@@ -44,6 +49,47 @@ public class Subscriber extends Observable implements Runnable {
     return this.data;
   }
 
+  private void writeCSV(String data) {
+	  if(null == file) {
+	    file = new File("Client/"+port+".csv"); 
+	    System.out.println("File Path:"+file.getAbsolutePath());
+	  }else {
+	    iter++;
+	    System.out.println("File exists:"+file.getName());
+	  }
+	  try { 
+	    String formattedData = data;
+	    formattedData = formattedData.replace('{', ' ');
+	    formattedData = formattedData.replace('}', ' ');
+	    formattedData = formattedData.replace('[', ' ');
+	    formattedData = formattedData.replace(']', ' ');
+	        FileWriter outputfile = new FileWriter(file,true); 
+	        CSVWriter csvWriter = new CSVWriter(outputfile); 
+	        String[] dataSplit = formattedData.split(",");
+	        String[] headers = new String[dataSplit.length];
+	        String[] rows = new String[dataSplit.length];
+	        if(iter == 0) {
+	          for(int k=0;k<dataSplit.length;k++) {
+	            if(k == 0) {
+	              headers[k] = "time";
+	              continue;
+	            }
+	            headers[k] = dataSplit[k].split("=")[0];
+	          }
+	          csvWriter.writeNext(headers); 
+	        }else {
+	          for(int k=0;k<rows.length;k++) {
+	            rows[k] = dataSplit[k].split("=")[1];
+	          }
+	          csvWriter.writeNext(rows); 
+	        }
+	        csvWriter.close(); 
+	    } 
+	    catch (IOException e) { 
+	        e.printStackTrace(); 
+	    } 
+	}
+  
   public void stop() {
     stop = true;
   }
@@ -76,6 +122,7 @@ public class Subscriber extends Observable implements Runnable {
       } else {
               System.out.println("in hello read and =" + stop + " " + measureLocal);
         setData(measureLocal);
+        writeCSV(measureLocal);
         setChanged();
         notifyObservers();
       }
@@ -102,5 +149,4 @@ public class Subscriber extends Observable implements Runnable {
     setChanged();
     notifyObservers();
   }
-
 }
